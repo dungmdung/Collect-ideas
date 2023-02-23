88992205a93fa8ddada65a3bfe5fbca18ce402c9
Bin Son <ndhs1609@gmail.com>
2023-02-23 22:48:03 +0700

    c

--- frontend/App/src/app/app.module.ts
+++ frontend/App/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './compoment/navbar/navbar.component';
+import { LoginComponent } from './compoment/login/login.component';
+import { MainComponent } from './compoment/main/main.component';
+import { FooterComponent } from './compoment/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
